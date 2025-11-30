cb2f7199ea517a7e71d0185086b2d757f7aa8dc9
devlife0409 <tkdghk2150@gmail.com>
2025-11-30 21:32:01 +0900

    [추가] d-day 계산 기능 추가

--- wd_guardian/src/components/Sec_calendar.svelte
+++ wd_guardian/src/components/Sec_calendar.svelte
@@ -2,7 +2,20 @@
   /*
   *@Overview 달력 & D Day count
   *@History  2025-10-30 / 미친토끼 / 최초생성
+  *@Modified 2025-11-30 / 이상화  / D-Day 계산 기능 추가
   */
+
+  // 결혼식 날짜: 2026년 02월 22일
+  const weddingDate = new Date('2026-02-22');
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // 시간을 00:00:00으로 설정하여 날짜 비교만 하도록
+
+  // 남은 일수 계산
+  const timeDiff = weddingDate.getTime() - today.getTime();
+  const daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+  // 음수 값 방지 (결혼식이 지났을 경우)
+  const ddayCount = daysLeft >= 0 ? daysLeft : 0;
 </script>
 
 <section class="sec sec-cal">
@@ -27,7 +40,7 @@
     </div>
 
     <span>의 결혼식이&nbsp;</span>
-    <span class="dday">00</span>
+    <span class="dday">{ddayCount}</span>
     <span>일&nbsp;남았습니다.</span>
 
   </div>
